6ab15dbe9486cf05479c4ae59af0af7bf14f9cbd
MaxxxMartinez <info@adultstack.com>
2015-09-22 02:27:47 +0800

    changed wp ==> base (untested)[issue 1]

--- config/application.php
+++ config/application.php
@@ -70,5 +70,5 @@ define('DISALLOW_FILE_EDIT', true);
  * Bootstrap WordPress
  */
 if (!defined('ABSPATH')) {
-  define('ABSPATH', $webroot_dir . '/wp/');
+  define('ABSPATH', $webroot_dir . '/base/');
 }
