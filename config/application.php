a069bf0631af0f4fc33e0d71aa03c1394b1efee2
MaxxxMartinez <info@adultstack.com>
2015-09-26 09:11:01 +0800

    change-wp [issue 1]

--- config/application.php
+++ config/application.php
@@ -45,7 +45,7 @@ define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
-$table_prefix = getenv('DB_PREFIX') ?: 'wp_';
+$table_prefix = getenv('DB_PREFIX') ?: 'ap_';
 
 /**
  * Authentication Unique Keys and Salts
